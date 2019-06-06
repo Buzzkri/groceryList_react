34b8ff0864bbad13c3a267f18d5d4805da9985b4
Aaron Krishnan <buzzkri@gmail.com>
2019-06-06 16:52:53 -0700

    Navbar added. Click function still broken

---
+++ src/GList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import List from './List';
+import GListFunction from './GListFunction';
+import GroceryListForm from './GroceryListForm';
+import { Container, } from "semantic-ui-react";
+
+const GList = (Func) => (
+  <GListFunction />
+
+  );
+
+
+export default GList;
--- src/GListFunction.js
+++ src/GListFunction.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import List from './List';
-import Home from "./Home";
-import About from "./About";
-import GList from "./GList";
-import Navbar from "./Navbar";
-import NoMatch from "./NoMatch";
 import GroceryListForm from './GroceryListForm';
-import { Route, Switch, } from "react-router-dom";
 import { Container, } from "semantic-ui-react";
+import GList from "./GList";
 
 
-class App extends React.Component {
+class GListFunction extends React.Component {
 
 
   state = {
@@ -39,24 +34,14 @@ class App extends React.Component {
     const { groceries } = this.state;
 
     return (
-      <>
-      <Navbar />
-
         <Container>
-          <Switch>    
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/GList" component={GList} />
-          <Route component={NoMatch} />
             <GroceryListForm addItem={this.addItem} />
             <List name="Grocery List" items={groceries} />
-          </Switch>
         </Container>
-      </>
     );
   }
 
  
 };
 
-export default App;
+export default GListFunction;
